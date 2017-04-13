e780060ca6264f977b4cc33f88554bd6c9bf99af
ljwleah <lujingweileah@yeah.net>
2017-04-13 16:58:55 +0800

    New changelist

--- src/main/java/com/bugdb/repository/BugRepository.java
+++ src/main/java/com/bugdb/repository/BugRepository.java
@@ -11,7 +11,7 @@ import com.bugdb.domain.Bug;
 import com.bugdb.domain.User;
 
 
-public interface BugRepository extends JpaRepository<Bug, Integer>, JpaSpecificationExecutor<User> {
+public interface BugRepository extends JpaRepository<Bug, Integer>, JpaSpecificationExecutor<Bug> {
 	@Query("select b from Bug b where bugNo = ?")
 	public Bug findByBugNo(Integer bugNo);
 	
@@ -19,9 +19,9 @@ public interface BugRepository extends JpaRepository<Bug, Integer>, JpaSpecifica
 	@Query("update Bug b set b.statusId=?1,b.assigned=?2,b.productId=?3,b.subject=?4 where b.bugNo=?5")
 	public int updateBug(int status,int assigned,int product,String subject,int bugNo);
 	
-	@Query("select b from Bug b where assigned = ?")
+	@Query("select b from Bug b where b.assigned = ?1")
 	public List<Bug> findByAssigned(int assigned);
 	
-	@Query("select b from Bug b where filedBy = ?")
+	@Query("select b from Bug b where b.filedBy = ?1")
 	public List<Bug> findByFiledBy(int filedBy);
 }
